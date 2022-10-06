49451f3fed80a3fe78119673d8da87fe0355fd39
JoshG232 <jcgeorgiou88@gmail.com>
2022-10-06 17:33:16 +0100

    styling of the nav

--- headerNav.php
+++ headerNav.php
@@ -1,28 +1,56 @@
+<?php
+    $loggedIn = "none";
+    $firstName = "";
+?>
 
+<style>
+    .navListItem2 {
+        display: <?php echo "$loggedIn" ?>;
+    }
+</style>
 
-    <ul class="navList">
-        <li class="navListItem"><a href="index.php" class="navListItemText">Home</a></li>
-        <li class="navListItem"><a href="loginRegister.php" class="navListItemText">Login/Registration</a></li>
-        <li class="navListItem"><a href="booking.php" class="navListItemText">Booking</a></li>
-        <li class="navListItem"><a href="report.php" class="navListItemText">Report</a></li>
-    </ul>
-    <ul class="navList2">
-        <li class="navListItem2"><a href="basket.php" class="navListItemText">Basket</a></li>
-        <li class="navListItem2"><a href="account.php" class="navListItemText">Account</a></li>
-        <!-- Once logged in need to change from "login/register" to "Account"  -->
-    </ul>
-
-    <?php
+<?php
     session_start();
 
     if (isset($_SESSION["firstName"])){
-        echo "<h1> Welcome " . $_SESSION["firstName"] . "</h1>";
-        echo '<a href="logout.php">Logout</a>';
+        $firstName = $_SESSION["firstName"];
+        $loggedIn = "unset";
+        $loggedIn2 = "none";
+        
         
     } else {
-        echo "<h1>Welcome Guest </h1>";
+        
         
     }
 
 ?>
 
+    <ul class="navList">
+        <li class="navListItem"><a href="index.php" class="navListItemText">Home</a></li>
+        
+        
+        <li class="navListItem"><a href="report.php" class="navListItemText">Report</a></li>
+        <li class="navListItem"><a href="content.php" class="navListItemText">Content</a></li>
+    </ul>
+    <ul class="navList2">
+        <li class="navListItem2"><a class="navListItemText"><?php echo "Welcome ". $firstName; ?> </a></li>
+        <li class="navListItem2"><a href="booking.php" class="navListItemText">Booking</a></li>
+        <li class="navListItem2"><a href="basket.php" class="navListItemText">Basket</a></li>
+        <li class="navListItem2"><a href="account.php" class="navListItemText">Account</a></li>
+        <li class="navListItem2"><a href="logout.php" class="navListItemText">Logout</a></li>
+
+        <li class="navListItem3"><a href="loginRegister.php" class="navListItemText">Login/Registration</a></li>
+
+        <!-- Once logged in need to change from "login/register" to "Account"  -->
+    </ul>
+
+    
+
+<style>
+    .navListItem2 {
+        display: <?php echo "$loggedIn" ?>;
+    }
+    .navListItem3 {
+        display: <?php echo "$loggedIn2" ?>;
+    }
+</style>
